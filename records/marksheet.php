c5ef507ccf53cef523646c9e0551c821d600c79e
adityasanil <aditya.sanil@somaiya.edu>
2019-04-17 23:32:33 +0530

    marksheet.php data update

--- records/marksheet.php
+++ records/marksheet.php
@@ -16,7 +16,7 @@ $rowSubjectMap = mysqli_fetch_array($querySubjectMap);
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Enter subject marks</title>
+    <title>Enter marks</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
